23e09452507005a5d292925673b9c6c5547b234f
IT21061134 <nawanjanayeshmi@gmail.com>
2023-04-18 12:49:01 +0530

    run the project

--- movies/src/main/java/me/nawanjana/movies/Review.java
+++ movies/src/main/java/me/nawanjana/movies/Review.java
@@ -1,4 +1,20 @@
 package me.nawanjana.movies;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
+
+    @Id
+    private ObjectId id;
+    private String body;
 }
